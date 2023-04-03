e9e659ffb562b4ffc5b2f0267a077f5df813b27d
Aria143 <129579777+Aria143@users.noreply.github.com>
2023-04-03 21:38:56 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,38 @@
+import openai
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler
+
+# Set up OpenAI API key
+openai.api_key = "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"
+
+# Set up Telegram bot token
+bot_token = "5935054751:AAG84Bhcweo3RyHjaljrs12hIEXYEHvfQxw"
+bot = telegram.Bot(token=bot_token)
+
+# Define the function that generates responses using OpenAI GPT-3
+def generate_response(text):
+    prompt = f"User: {text}\nAI:"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].text.strip()
+
+# Define the function that handles incoming messages from Telegram
+def handle_message(update, context):
+    user_text = update.message.text
+    bot_text = generate_response(user_text)
+    update.message.reply_text(bot_text)
+
+# Set up the Telegram bot updater and handlers
+updater = Updater(bot_token, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(telegram.ext.Filters.text & ~telegram.ext.Filters.command, handle_message))
+
+# Start the Telegram bot
+updater.start_polling()
